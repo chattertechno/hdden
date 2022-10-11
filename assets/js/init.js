6d77921932c1813d61cddf4746b11c52aa4e4c4c
alexthecurator <edge.works.curator@gmail.com>
2022-10-11 09:45:14 +0300

    Refactoring as clients requests

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,4 @@
-
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 /* Parámetro de posición ---------------------------------------------
 |   <   :insertar en el INICIO de la animación anterior
@@ -18,48 +17,54 @@ gsap.registerPlugin(ScrollTrigger)
 //https://greensock.com/position-parameter
 
 jQuery(function () {
+  let cursor = $(".cursor"),
+    follower = $(".cursor__follower"),
+    scroll__down = $(".scroll__down"),
+    nav = $(".nav"),
+    navbar__logohdden = $(".navbar__logohdden"),
+    navbar__menuTogle = $(".navbar__menu-togle"),
+    params = new URLSearchParams(window.location.search),
+    to__collection = $(".to__collection"),
+    btn_conectToWallet = $("a.nav__item--wallet");
+  video_wrap = $(".video_wrap");
+  seccion2 = $(".seccion2");
 
-    let cursor = $(".cursor"),
-        follower = $(".cursor__follower"),
-        scroll__down = $(".scroll__down"),
-        nav = $(".nav"),
-        navbar__logohdden = $(".navbar__logohdden"),
-        navbar__menuTogle = $('.navbar__menu-togle'),
-        params = new URLSearchParams(window.location.search),
-        to__collection = $(".to__collection"),
-        btn_conectToWallet = $('a.nav__item--wallet')
+  if (params.has("collection")) {
+    video_wrap.addClass("hide");
+    seccion2.addClass("seccion2_backdrop");
+    scrollToSection(".seccion2", 6);
+  }
 
-    if (params.has('collection')) scrollToSection('.seccion2', 6)
+  to__collection.on("click", () => {
+    video_wrap.addClass("hide");
+    seccion2.addClass("seccion2_backdrop");
+    scrollToSection(".seccion2", 6);
+  });
 
-    to__collection.on('click', () => scrollToSection('.seccion2', 6))
+  navbar__logohdden.on("click", () => scrollToSection(0));
 
-    navbar__logohdden.on("click", () => scrollToSection(0))
+  navbar__menuTogle.click(function () {
+    nav.toggleClass("mobile__nav");
+    $(this).toggleClass("is-active");
+  });
 
-    navbar__menuTogle.click(function () {
-        nav.toggleClass("mobile__nav");
-        $(this).toggleClass("is-active");
-    })
+  btn_conectToWallet.on("click", function () {
+    $(this).toggleClass("active");
 
-    btn_conectToWallet.on('click', function () {
-        $(this).toggleClass("active")
-
-        if($(this).hasClass("active"))
-            $(this).html('').html('<span>isxO98....8786</span>')
-        else
-            $(this).html('').html('<span>Connect to Wallet</span>')
-    })
-
-
-})
+    if ($(this).hasClass("active"))
+      $(this).html("").html("<span>isxO98....8786</span>");
+    else $(this).html("").html("<span>Connect to Wallet</span>");
+  });
+});
 
 // Functions -----------------------------------
 
 function scrollToSection(param, seconds = 2.5) {
-    gsap.to(window, {
-        duration: seconds,
-        delay: 0.6,
-        scrollTo: {
-            y: param,
-        }
-    });
+  gsap.to(window, {
+    duration: seconds,
+    delay: 0.6,
+    scrollTo: {
+      y: param,
+    },
+  });
 }
