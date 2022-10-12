12934dd97a215b560d76ce3fe7654b211f85205e
alexthecurator <edge.works.curator@gmail.com>
2022-10-12 14:02:30 +0300

    Animation fix

--- assets/js/hddenforms/index.js
+++ assets/js/hddenforms/index.js
@@ -232,8 +232,8 @@ function animateMovimientoLogo(elemento, noX, noY, time) {
       x: randomX(-1),
       y: randomX(1),
     });
-    moveX(item, 1);
-    moveY(item, -1);
+    moveX(item, 3);
+    moveY(item, -7);
   });
 
   function moveX(target, direction) {
